baa1cf6dbeb120035ccc4ba6aad1e09d057b4618
shapimati <mschapiro@sappco.com>
2014-09-01 15:37:14 -0300

    prueba1
    prueba1

---
+++ SurveyQuestion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace MetroAppDemo.Classes
+{
+    public class SurveyQuestion
+    {
+        public SurveyQuestion(JObject jSubMessage)
+        {
+            questionId = getValue((String)jSubMessage["questionId"]);
+            question = getValue((String)jSubMessage["question"]);
+            questionType = getValue((String)jSubMessage["questionType"]);
+
+            possibleAnswers = new List<string>();
+
+            if (jSubMessage["possibleAnswers"] != null)
+            {
+                foreach (JObject answers in jSubMessage["possibleAnswers"])
+                {
+                    possibleAnswers.Add(answers["answer"].ToString());
+                }
+            }
+        }
+
+        public String questionId { get; set; }
+        public String question { get; set; }
+
+        public String questionType { get; set; }
+        public List<String> possibleAnswers { get; set; }
+
+        private String getValue(String value)
+        {
+            if (value != null) return value;
+
+            return "";
+        }
+
+
+    }
+}
